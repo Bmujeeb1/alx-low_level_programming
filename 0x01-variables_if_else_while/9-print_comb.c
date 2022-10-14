c5e4923a53bf51fbacb0a4d1343161caad3c505d
Bmujeeb1 <babdulmujeeb@gmail.com>
2022-10-14 15:57:28 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9 seperated by a comma followed by a space
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = '0';
+
+	while (number <= '9')
+	{
+		putchar(num);
+		if (number != '9')
+		{
+			putchar(',);
+			puchar(' ');
+		}
+		number++;
+	}
+	putchar('\n');
+
+	return (0);
+}
