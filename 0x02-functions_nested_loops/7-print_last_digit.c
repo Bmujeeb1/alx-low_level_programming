eb5f54f25bd600b541923879e851a14a1dc59424
Bmujeeb1 <babdulmujeeb@gmail.com>
2022-10-18 16:30:25 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,16 @@ int print_last_digit(int n)
 	int ld = n % 10;
 
 	if (ld < 0)
-		ld *= -1;
+	{
+		ld = -(ld);
 
-	_putchar(ld + '0');
-
+		_putchar(ld + '0');
+		return (ld);
+	}
+	else
+	{
+		_putchar(ld + '0');
+		return (ld);
+	}
 	return (0);
 }
